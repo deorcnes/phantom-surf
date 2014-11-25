f3cb608f5243f160fd24e8593fcd35a9c4f4d1e6
active6 <bob@active6.com>
2014-11-25 22:56:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+Heroku PhantomSurf
+==================
+
+This is a one-click deploy app for remote browsing on Heroku using PhantomJS.
+
+Usage
+-----
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+Click this button to deploy this app to Heroku.
+
+If you don't have a Heroku account yet, you'll have the option to set one up for free.
+
+The app will be added to your Heroku Dashboard with a random name (you can change this name in the Heroku settings page)
+
+Open the app details in your Heroku Dashboard and click the "Open App" icon (small link icon on the top of your app details page)
+
+This will open the link in your browser, it will look something like:
+
+http://apple-orange-9876.herokuapp.com/
+
+Simply add the destination URL to this in your browser, for example:
+
+http://apple-orange-9876.herokuapp.com/?url=http://google.com/
+
+The Phantom browser on your Heroku instance will load the URL and send back a screen shot
+
+You can refresh the page to get updated screen shots.
+
---
+++ surf.js
@@ -0,0 +1,91 @@
+var page = require('webpage').create();
+var server = require('webserver').create();
+var system = require('system');
+var fs = require("fs");
+var host, port;
+var address, output, size;
+var currentAddress = null;
+
+page.onConsoleMessage = function (msg) 
+{
+    console.log('---> Page console log: ' + msg);
+};
+
+port = system.args[1] || system.env.PORT;
+
+var listening = server.listen(port, function (request, response) 
+{
+    console.log("GOT HTTP REQUEST ("+request.method+")");
+
+    var a = 0;
+    if (request.method === "GET") {
+    // GET request
+        if((a=request.url.indexOf("url=")) > 0) 
+		{
+            console.log("Loading URL " + request.url.slice(a+4));
+            var params = URLToArray(request.url);
+            output = "g.png";
+            page.viewportSize = { width: 800, height: 600 };
+			if(currentAddress == null)
+			{
+				address = params["url"];
+				page.open(address, function (status) {
+					if (status !== 'success') {
+						console.log('Unable to load the address!');
+					} else {
+						currentAddress = address;
+						window.setTimeout(function () {
+							var png = page.renderBase64("PNG");
+							response.statusCode = 200;
+							response.headers = {"Cache": "no-cache", "Content-Type": "text/html"};
+							response.write("<html><head><title>Page Loaded</title></head><body>");
+							response.write('<img src="data:image/png;base64,'+png+'"/>');
+							response.write("</body></html>");
+							response.close();
+						}, 200);
+					}
+				});            
+			}
+			else
+			{
+				window.setTimeout(function () {
+					var png = page.renderBase64("PNG");
+					response.statusCode = 200;
+					response.headers = {"Cache": "no-cache", "Content-Type": "text/html"};
+					response.write("<html><head><title>Page Shot</title></head><body>");
+					response.write('<img src="data:image/png;base64,'+png+'"/>');
+					response.write("</body></html>");
+					response.close();
+				}, 200);			
+			}
+        } else {
+             // we set the headers here
+            response.statusCode = 200;
+            response.headers = {"Cache": "no-cache", "Content-Type": "text/html"};
+            // this is also possible:
+            response.setHeader("foo", "bar");
+            // now we write the body
+            // note: the headers above will now be sent implictly
+            response.write("<html><head><title>PhantomSurf</title></head>");
+            // note: writeBody can be called multiple times
+            response.write("<body><p>Specify a URL to surf to</body></html>");
+            response.close();       
+        }
+    } 
+});
+if (!listening) {
+    console.log("could not create web server listening on port " + port);
+    phantom.exit();
+}
+
+function URLToArray(url) 
+{
+  var request = {};
+  var pairs = url.substring(url.indexOf('?') + 1).split('&');
+  for (var i = 0; i < pairs.length; i++) {
+    var pair = pairs[i].split('=');
+    request[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+  }
+  return request;
+}
+ 
